95f743adeffcae52c543915b60cee1a88cf16a6a
Albert Ong <albertong.indocart@gmail.com>
2018-07-22 13:49:26 +0700

    update user

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
+var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
+var validator = require('express-validator');
 
 var indexRouter = require('./routes/index');
+var userRoutes = require('./routes/user');
 
 var app = express();
 
@@ -14,7 +19,8 @@ var app = express();
 mongoose.connect("mongodb://localhost:27017/shopping", { useNewUrlParser: true })
   .then(() => console.log('connection successful'))
   .catch((err) => console.log(err));
-  
+require ('./config/passport');
+
 // view engine setup
 app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
 app.set('view engine', '.hbs');
@@ -24,13 +30,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(validator());
+app.use(session({secret: 'mysupersecret',resave:false, saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use(function(req,res,next) {
+  res.locals.login=req.isAuthenticated();
+  next();
+});
+
+app.use('/user', userRoutes);
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   var err = new Error ('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 // error handler
 app.use(function (err, req, res, next) {
---
+++ models/user.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt   = require('bcrypt-nodejs');
+
+var userSchema = new Schema({
+    email        : {type: String, required: true},
+    password     : {type: String, required: true},
+});
+
+// methods ======================
+// generating a hash
+userSchema.methods.encryptPassword = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
